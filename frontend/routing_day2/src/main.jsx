6984c5ec5e208d98bb6ff24dee57f46728db3de4
ganeshbala2k3 <ganeshbala2k3@gmail.com>
2026-03-05 11:51:14 +0530

    updated

--- frontend/routing_day2/src/main.jsx
+++ frontend/routing_day2/src/main.jsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import First from './Components/first.jsx'
 import Second from './Components/second.jsx'
 import Third from './Components/third.jsx'
